5c9aae649347344fd5931eebf312bf674f06d958
malikmukhtar <malikmukhtar63@gmail.com>
2024-08-26 16:45:39 +0100

    contact form

--- js/main.js
+++ js/main.js
@@ -241,6 +241,29 @@
         // Call the fetchData function on page load
         fetchData();
     });
+
+    function sendMail() {
+        var params = {
+          name: document.getElementById("name").value,
+          email: document.getElementById("email").value,
+          message: document.getElementById("message").value,
+        };
+      
+        const serviceID = "service_hsldqp6";
+        const templateID = "template_wh5x4ao";
+      
+          emailjs.send(serviceID, templateID, params)
+          .then(res=>{
+              document.getElementById("name").value = "";
+              document.getElementById("email").value = "";
+              document.getElementById("message").value = "";
+              console.log(res);
+              alert("Your message sent successfully!!")
+      
+          })
+          .catch(err=>console.log(err));
+      
+      }
     
 
 
